a500da241279e4d1df5a2848f2cbbebec65b3ae9
Pratish bhongle <bhonglepratish@gmail.com>
2025-01-24 17:49:55 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=144553
+let accountEmail="bhonglepratish@gmail.com"
+var accountPrivacy="12345"
+accountCity="jaipur"
+
+
+//accountId=2// not allowed
+console.log(accountId)
+console.table([accountId,accountEmail,accountPrivacy,accountCity])
