65c160711455fa42895d6d8091821e014bc51c81
Pratish bhongle <bhonglepratish@gmail.com>
2025-01-25 05:25:15 +0000

    data types summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,36 @@
+// primitive
+
+//7 types :String,Number,Boolean,null,undefined,bigint,symbol.
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const OutsideTemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id===anotherId);
+
+constbigNumber=3456543576654356754n
+
+// Refrence (non primitive)
+
+//Array,Objects,Functions
+
+const heros=["saktiman","naagraj","doga"]
+let myObj={
+    name:"pratish",
+    age:20,
+}
+
+const myFunction=function(){
+    console.log("hello world");
+    
+}
+
+console.log(typeof heros);
+
+//link to learn-https:262.ecma-international.org/5.1/#sec-11.4.3//
