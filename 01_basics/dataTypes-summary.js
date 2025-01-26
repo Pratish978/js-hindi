a7383db13e85d1e4299d635686b7ffc6257fc79a
Pratish bhongle <bhonglepratish@gmail.com>
2025-01-26 10:01:15 +0000

    summary

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -34,3 +34,31 @@ const myFunction=function(){
 console.log(typeof heros);
 
 //link to learn-https:262.ecma-international.org/5.1/#sec-11.4.3//
+
+
+
+
+////// memory////
+
+//Stack (primitive) ,Heap(non primitive)
+
+let myYoutubename="pratish"
+
+let anothername=myYoutubename
+anothername="pratish123"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne={
+    email:"bhonglepratish@gmail.com",
+    upi:"pratishbhongle@okaxis",
+}
+
+let userTwo=userOne
+
+userTwo.email="pratish.bhongle@universal.edu.in"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
